539811a27ad78ba328857f5dd554279d702fa67b
Riddhima Deshmukh <115169787+ridds-io@users.noreply.github.com>
2026-03-26 03:12:59 +0530

    Update BudgetingCityView.tsx

--- components/BudgetingCityView.tsx
+++ components/BudgetingCityView.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState, useCallback } from 'react';
+import { useEffect, useState, useCallback, useRef } from 'react';
 import dynamic from 'next/dynamic';
 import { useRouter } from 'next/navigation';
 import {
